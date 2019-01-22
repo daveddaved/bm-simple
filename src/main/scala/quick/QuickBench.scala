5ec74549e28c81cec0a08a9fe5e34377f42c4ab8
ddikker <david.dikker@gmail.com>
2019-01-21 21:46:45 -0500

    bm

--- src/main/scala/quick/QuickBench.scala
+++ src/main/scala/quick/QuickBench.scala
@@ -1,5 +1,6 @@
 package quick
 import org.openjdk.jmh.annotations.{Benchmark, Scope, State}
+import scalqa.Stream
 
 @State(Scope.Thread)
 class QuickBench {
@@ -8,8 +9,9 @@ class QuickBench {
 
    private val testMedXS: List[Int] = (1 to 1000000).toList
    private val testMedlVec: Vector[Int] = testMedXS.toVector
+  private val st: scalqa.Stream[Int] = Stream.from(testMedXS)
 
-   @Benchmark def mapSmallList: List[Int] = testSmallXS.map(_ + 1)
    @Benchmark def mapMedList: List[Int] = testMedXS.map(_ + 1)
+   @Benchmark def mapMedStream: Stream[Int] = st.map(_ + 1)
 
 }
